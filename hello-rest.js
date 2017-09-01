de30c7b723a2ddbdb0957281347dda70bcba3ee2
varun.dhanraj.mishra <varun.dhanraj.mishra@accenture.com>
2017-09-01 11:43:49 +0530

    Updated greet message 3

--- hello-rest.js
+++ hello-rest.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from FRA Kubernetes Cluster');
+  res.end('Hello from fabric8');
 }).listen(8080);
 console.log('Server running at http://localhost:8080/');
